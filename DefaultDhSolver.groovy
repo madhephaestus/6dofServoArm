e4b966684cde1f623acd44221d353e5fb0c4deb1
Kevin Harrington <mad.hephaestus@gmail.com>
2022-09-11 17:32:52 -0400

    more testing for wrist options

--- DefaultDhSolver.groovy
+++ DefaultDhSolver.groovy
@@ -249,24 +249,41 @@ public class scriptJavaIKModel implements DhInverseSolver {
 		//println "Law of cosines results "+shoulderTiltAngle+" and "+elbowTiltAngle
 		return jointSpaceVector;
 	}
-
+	
+	double[] option(double w1,double w2,double w3) {
+		return [w1,w2,w3] as double[]
+	}
+	
 	double[] normalize(double[] calculated,double[] current, DHChain chain) {
 		AbstractKinematicsNR kin = chain.kin;
 		DecimalFormat df = new DecimalFormat("000.00");
-		double[] alt1 = [calculated[0]-180,-calculated[1],calculated[2]-180]as double[]
-		HashMap<double[],Double> scores= new HashMap<>();
-		score(calculated,current,scores,kin)
-		score(alt1,current,scores,kin)
-		score(calculated.collect{it+360}as double[],current,scores,kin)
-		score(alt1.collect{it+360}as double[],current,scores,kin)
-		score(calculated.collect{it-360}as double[],current,scores,kin)
-		score(alt1.collect{it-360}as double[],current,scores,kin)
-		score([calculated[0]-360,calculated[1],calculated[2]]as double[],current,scores,kin)
-		score([calculated[0]+360,calculated[1],calculated[2]]as double[],current,scores,kin)
 		
-		score([alt1[0]-360,alt1[1],alt1[2]]as double[],current,scores,kin)
-		score([alt1[0]+360,alt1[1],alt1[2]]as double[],current,scores,kin)
-
+		double[] alt1 =option( calculated[0] - 180, -calculated[1], calculated[2] - 180 );
+		double[] calculated2 =option( calculated[0] + 360, calculated[1] + 360, calculated[2] + 360 );
+		double[] calculated3 =option( calculated[0] - 360, calculated[1] - 360, calculated[2] - 360 );
+		double[] alt2 =option( alt1[0] + 360, alt1[1] + 360, alt1[2] + 360 );
+		double[] alt3 =option( alt1[0] - 360, alt1[1] - 360, alt1[2] - 360 );
+		double[] calculated6 =option( calculated[0] - 360, calculated[1], calculated[2] );
+		double[] calculated7 =option( calculated[0] + 360, calculated[1], calculated[2] );
+		double[] als4 =option( alt1[0] - 360, alt1[1], alt1[2] );
+		double[] alt5 =option( alt1[0] + 360, alt1[1], alt1[2] );
+		
+		HashMap<double[], Double> scores = new HashMap<>();
+		score(calculated, current, scores, kin);
+		score(alt1, current, scores, kin);
+		score(calculated2, current, scores, kin);
+		score(calculated3, current, scores, kin);
+		score(alt2, current, scores, kin);
+		score(alt3, current, scores, kin);
+		score(calculated6, current, scores, kin);
+		score(calculated7, current, scores, kin);
+		score(als4, current, scores, kin);
+		score(alt5, current, scores, kin);
+		score(option( calculated[0] -180, -calculated[1], calculated[2]+180 ), current, scores, kin);
+		score(option( alt1[0] -180, alt1[1], -alt1[2]+180 ), current, scores, kin);
+		score(option( calculated[0] +180, -calculated[1], calculated[2]-180 ), current, scores, kin);
+		score(option( alt1[0] +180, -alt1[1], alt1[2]-180 ), current, scores, kin);
+		
 		if(scores.size()>0) {
 			double[] start =calculated ;
 			if(scores.get(start)==null) {
@@ -274,22 +291,22 @@ public class scriptJavaIKModel implements DhInverseSolver {
 			}
 			double score=scores.get(start);
 			double[] ret=start;
-			//println "\n\n"
+			println "\n\n"
 			for(double[]  tmp:scores.keySet()) {
 				double delt =scores.get(tmp)
-				//println ""+tmp.collect{df.format(it)}+" score "+delt+" cur "+current.collect{df.format(it)}
+				println ""+tmp.collect{df.format(it)}+" score "+delt+" cur "+current.collect{df.format(it)}
 				if(delt<score) {
 					score=delt
 					ret=tmp
-					//println "Best yet"
+					println "Best yet"
 				}
 			}
 	//		if(ret!=calculated)
 	//			println "Current "+current.collect{df.format(it)}+" Normalizing wrist from:\n"+calculated.collect{df.format(it)+"\t"}+"\nto:\n"+ret.collect{df.format(it)+"\t"}
 			return ret
 		}
-		//println("No Solution! "+calculated.collect{df.format(it)}+" cur "+current.collect{df.format(it)})
-		
+		println("No Solution! "+calculated.collect{df.format(it)}+" cur "+current.collect{df.format(it)})
+		throw new RuntimeException("No Wrist Solution! ");
 		return current
 	}
 	
@@ -299,10 +316,10 @@ public class scriptJavaIKModel implements DhInverseSolver {
 			int i3 = i+3;
 			calculated[i]=calculated[i] % 360;
 			if(calculated[i] >kin.getMaxEngineeringUnits(i3)) {
-				calculated[i]=kin.getMaxEngineeringUnits(i3);
+				return //calculated[i]=kin.getMaxEngineeringUnits(i3);
 			}
 			if(calculated[i] <kin.getMinEngineeringUnits(i3)) {
-				calculated[i]=kin.getMinEngineeringUnits(i3)
+				return //calculated[i]=kin.getMinEngineeringUnits(i3)
 			}
 			double measure = current[i]-calculated[i];
 			if(Math.abs(measure)>Math.abs(delt)) {
